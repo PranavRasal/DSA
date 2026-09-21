d91323c5b0e0dedb34be34617e423179857a59c2
Pranav Rasal <pranavcode19@gmail.com>
2026-09-22 01:18:47 +0530

    Time: 901 ms (5.02%), Space: 177.4 MB (5.27%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -7,7 +7,7 @@ public:
         set<int> s(nums.begin() , nums.end()) ;
         if(valueDiff == 0 && n == s.size()){
             return false ;
-        }
+        } // cheating 
         for(int i = 0 ; i < n ; i++){
             for(int j = i + 1 ; j < i + 1 + indexDiff && j < n ; j++){
                 long long ans = abs(nums[i] - nums[j]) ;
